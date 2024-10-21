7c5748e0803b7c0abedbdec26cbc9b7a3a19c853
Fredysuwanto <fredysuwanto56@gmail.com>
2024-10-21 23:34:06 +0700

    push

--- wisata_candi/lib/screens/detail_screen.dart
+++ wisata_candi/lib/screens/detail_screen.dart
@@ -2,15 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:wisata_candi/models/candi.dart';
 
 class DetailScreen extends StatelessWidget{
-  const DetailScreen ({super.key, required Candi candi});
+  const DetailScreen ({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-
+          Stack(
+            children: [
+              //images/
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    candi.imageAsset,
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[100]?.withOpacity(0.8),
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(onPressed: () {} , icon: const Icon(
+                    Icons.arrow_back,
+                  ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
+
       ),
     );
   }
